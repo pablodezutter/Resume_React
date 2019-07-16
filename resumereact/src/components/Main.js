c8556452a76f76ba811bd2eb1fc8f9bec3a8eaa2
pablodezutter <dezutterpablo@gmail.com>
2019-07-16 17:00:05 +0200

    place picture

--- resumereact/src/components/Main.js
+++ resumereact/src/components/Main.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "../css/style.css";
+import fotoPablo from "../Pictures/fotoPablo.jpg";
 
 function Main() {
   return (
     <div className="mainContent">
       <h1>Hello!</h1>
-      <p>This is a React app.</p>
+
+      <div className="leftSide">
+        This is a React app.
+        <img src={fotoPablo} alt="fotoofjuniordeveloper" />
+      </div>
+      <div className="rightSide">
+        {" "}
+        <p>Skills:</p>
+        <ol>
+          <li>HTML</li>
+          <li>Javascript</li>
+          <li>Css</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+        </ol>
+        <p>Points of interest: nature, photography,wooddesign, gardens </p>
+      </div>
     </div>
   );
 }
